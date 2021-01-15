9e56764696fc33f7ff46c6d9882b55ac8921300e
Daniel Norred <daniel@nor.red>
2021-01-14 18:41:39 -0600

    Add cmdlets to module

--- src/Users/Get-WebexTeamsUserDetails.ps1
+++ src/Users/Get-WebexTeamsUserDetails.ps1
@@ -1,14 +1,11 @@
 function Get-WebexTeamsUserDetails {
     param (
-        $id,
-        [Parameter(ValueFromPipeline=$true)]
-        $inputObject
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $id
     )
-    if($null -eq $id) {$id -eq $inputObject.id}
-
     $res = Invoke-RestMethod -Headers (Get-WebexTeamsCredential) `
         -ContentType "application/json" `
         -uri "https://webexapis.com/v1/people/$id" `
 
-    return $res
+    return $res.items
 }
