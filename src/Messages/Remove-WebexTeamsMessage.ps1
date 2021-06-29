cf000b635ca1e8e2bd98aa8adf7c09159adf2035
Caleb Bartle <calebbartle@gmail.com>
2021-06-29 13:58:23 -0500

    Updating with $name

--- src/Messages/Remove-WebexTeamsMessage.ps1
+++ src/Messages/Remove-WebexTeamsMessage.ps1
@@ -2,10 +2,11 @@ function Remove-WebexTeamsMessage {
     param (
         [Parameter(ValueFromPipelineByPropertyName, mandatory = $true)]
         [alias('id')]
-        [string]$messageId
+        [string]$messageId,
+        [string]$name
     )
     try {
-        $res = Invoke-RestMethod -Headers (Get-WebexTeamsCredential) `
+        $res = Invoke-RestMethod -Headers (Get-WebexTeamsCredential -name $name) `
             -ContentType "application/json" `
             -uri "https://webexapis.com/v1/messages/$messageId" `
             -Method Delete
